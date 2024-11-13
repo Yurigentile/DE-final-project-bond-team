a2ecdaffa1ae3125548180f2e0ce343aa646f025
bengriffiths95 <bengriffiths95@gmail.com>
2024-11-13 12:28:09 +0000

    add create_conn utility function with passing test file

--- src/db_connection.py
+++ src/db_connection.py
@@ -2,22 +2,40 @@ from pg8000.native import Connection
 from src.secrets_manager import get_secret
 import json
 
-# Create your create_conn function to return a database connection object
-def create_conn(sm_params):
-    db_params = json.loads(sm_params)
-    print(sm_params)
-    print(db_params)
-    # try:
-    #     conn = Connection(
-    #         cohort_id = db_params['cohort_id'],
-    #         database=db_params['database'],
-    #         user=db_params['user'],
-    #         password=db_params['password'],
-    #         host = db_params['host'],
-    #         port = db_params['port']
-    #     )
-    #     return conn
 
-    # except Exception as e:
-    #     print (f' Something went wrong: {e}')
+def create_conn(sm_params):
+    """
+    Returns pg8000 database connection
+
+    Parameters:
+        sm_params (json): JSON object containing the database credentials. 
+
+    Returns:
+        pg8000 connection object
+
+    Raises:
+        Exception: Any exception raised by pg8000 will be printed
+
+    Side Effects:
+        Outputs 'Connected to database' message upon successful connection, or error upon exception
+
+    Example usage with get_secret util function:
+    >>> secrets = get_secret('totes-database')
+    >>> conn = create_conn(secrets)
+    """
+    try:
+        db_params = json.loads(sm_params)
+        conn = Connection(
+            database=db_params['database'],
+            user=db_params['user'],
+            password=db_params['password'],
+            host = db_params['host'],
+            port = db_params['port'],
+            timeout = 10
+        )
+        print(f"Connected to database {db_params['database']}")
+        return conn
+
+    except Exception as e:
+        print (f' Something went wrong: {e}')
     
