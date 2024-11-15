4e03dcc65fc7cfe615436030d5fe9697d9292959
bengriffiths95 <bengriffiths95@gmail.com>
2024-11-15 14:22:06 +0000

    fix issues with lambda function deployment with new folder structure for handler & util functions

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,17 +1,12 @@
 
-#Zipping functions and layers
+#Zipping handler function and dependency layer
 data "archive_file" "lambda" {
   type             = "zip"
   output_file_mode = "0666"
-  source_file      = "${path.module}/../extract_lambda/handler.py"
+  source_dir      = "${path.module}/../extract_lambda"
   output_path      = "${path.module}/../extract_lambda.zip"
 }
-data "archive_file" "layer_util_functions" {
-  type             = "zip"
-  output_file_mode = "0666"
-  source_dir       = "${path.module}/../extract_lambda"
-  output_path      = "${path.module}/../extract_util_functions.zip"
-}
+
 data "archive_file" "layer_dependencies" {
   type             = "zip"
   output_file_mode = "0666"
@@ -19,37 +14,22 @@ data "archive_file" "layer_dependencies" {
   output_path      = "${path.module}/../dependencies.zip"
 }
 
-
-
-#Uploading them to the bucket
-resource "aws_s3_object" "file_upload_dependencies" {
-  bucket = "${aws_s3_bucket.code.id}"
-  key    = "layers/dependencies.zip"
-  source = "${path.module}/../dependencies.zip"
-}
-
-resource "aws_s3_object" "file_upload_util_functions" {
-  bucket = "${aws_s3_bucket.code.id}"
-  key    = "layers/utilities.zip"
-  source = "${path.module}/../extract_util_functions.zip"
-}
-
+#Uploading them to the code S3 bucket
 resource "aws_s3_object" "file_upload_extract_lambda" {
   bucket = "${aws_s3_bucket.code.id}"
   key    = "lambda-functions/extract_lambda.zip"
   source = "${path.module}/../extract_lambda.zip"
+  # source_hash   = filemd5("${path.module}/../extract_lambda.zip")
 }
 
-
-
-#Request layers from the bucket
-resource "aws_lambda_layer_version" "requests_layer_util_functions" {
-  layer_name          = "requests_layer_util_functions"
-  compatible_runtimes = [var.python_runtime]
-  s3_bucket           = aws_s3_bucket.code.bucket
-  s3_key              = aws_s3_object.file_upload_util_functions.key
+resource "aws_s3_object" "file_upload_dependencies" {
+  bucket = "${aws_s3_bucket.code.id}"
+  key    = "layers/dependencies.zip"
+  source = "${path.module}/../dependencies.zip"
+  # source_hash   = filemd5("${path.module}/../dependencies.zip")
 }
 
+#Deploying layer
 resource "aws_lambda_layer_version" "requests_layer_dependencies" {
   layer_name          = "requests_layer_dependencies"
   compatible_runtimes = [var.python_runtime]
@@ -57,7 +37,7 @@ resource "aws_lambda_layer_version" "requests_layer_dependencies" {
   s3_key              = aws_s3_object.file_upload_dependencies.key
 }
 
-
+#Deploying lambda function
 resource "aws_lambda_function" "extract_handler" {
   s3_bucket     = aws_s3_bucket.code.bucket
   s3_key        = "lambda-functions/extract_lambda.zip"
@@ -65,7 +45,8 @@ resource "aws_lambda_function" "extract_handler" {
   role          = aws_iam_role.lambda_role.arn
   handler       = "handler.lambda_handler"
   runtime       = var.python_runtime
-  layers        = [aws_lambda_layer_version.requests_layer_dependencies.arn,aws_lambda_layer_version.requests_layer_util_functions.arn]
+  layers        = [aws_lambda_layer_version.requests_layer_dependencies.arn]
+  source_code_hash = data.archive_file.lambda.output_base64sha256
 } 
 
 
