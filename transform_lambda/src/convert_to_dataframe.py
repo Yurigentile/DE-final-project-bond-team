6bcf5436053054f04ab4c05adfa4772c59c883be
bengriffiths95 <bengriffiths95@gmail.com>
2024-11-22 15:44:56 +0000

    add in-progress test_transform_handler with happy path handling added to convert_to_dataframe

--- transform_lambda/src/convert_to_dataframe.py
+++ transform_lambda/src/convert_to_dataframe.py
@@ -6,6 +6,7 @@ def convert_dictionary_to_dataframe(data_dict):
         new_dict = {}
         for table in data_dict:
             new_dict[table] = pd.DataFrame(data_dict[table])
+        print('Dict converted to DataFrame')
         return new_dict
     except Exception as e:
         print(f"Error: {e}")
--- transform_lambda/transform_handler.py
+++ transform_lambda/transform_handler.py
@@ -11,31 +11,58 @@ else:
     # For local use
     from transform_lambda.src.load_new_data import load_new_data
     from transform_lambda.src.convert_to_dataframe import convert_dictionary_to_dataframe
-    from transform_lambda.src.df_to_parquet import convert_dataframes_to_parquet
+    from transform_lambda.src.df_to_parquet import convert_dataframe_to_parquet
 
 # EVENT/CONTEXT
     # triggered off previous lambda function's completion? Or files uploaded to S3 bucket? 
     # event includes data bucket name and processed bucket name
 
+# TEMP - remove after transform functions done
+def transform_function(df):
+    pass
+
 def lambda_handler(event, context):
-    logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
+    try:
+        logger = logging.getLogger()
+        logger.setLevel(logging.INFO)
 
-    data_bucket = event.get("data_bucket")
-    processed_bucket = event.get("processed_bucket")
 
-    # load new JSON files from data bucket + return nested dictionary
-    extracted_data_dict = load_new_data(data_bucket)
+        data_bucket = event.get("data_bucket")
+        processed_bucket = event.get("processed_bucket")
 
-    # convert dictionaries inside extracted_data_dict into dataframes
-    extracted_data_df = convert_dictionary_to_dataframe(extracted_data_dict)
+        logger.info("Passed event: data_bucket=%s, processed_bucket=%s", data_bucket, processed_bucket)
 
-    # create a blank dict to store the transformed dataframes 
-    transformed_data_df = {}
+        tables = [
+            "design",
+            "sales_order",
+            "staff",
+            "currency",
+            "counterparty",
+            "address",
+            "department",
+            "purchase_order",
+            "payment_type",
+            "payment",
+            "transaction",
+        ]
 
-    # invoke each transform function, passing the relevant dataframe from extracted_data_df as an arg and saving this as a value on a key in transformed_dfs
-    transformed_data_df['sales_order_fact'] = EXAMPLE___transform_sales_order(extracted_data_df['sales_order'])
+        # load new JSON files from data bucket + return nested dictionary
+        extracted_data_dict = load_new_data(data_bucket, tables)
 
-    # loop through the dataframes in transformed_dfs, passing the table name, dataframe & target bucket into convert_dataframes_to_parquet to be convert to parquet and save to processed bucket
-    for table_name, dataframe in transformed_data_df.items():
-        convert_dataframes_to_parquet(table_name, dataframe, processed_bucket)
+        # convert dictionaries inside extracted_data_dict into dataframes
+        if extracted_data_dict:
+            extracted_data_df = convert_dictionary_to_dataframe(extracted_data_dict)
+        else:
+            print('nothing ')
+
+        # create a blank dict to store the transformed dataframes 
+        transformed_data_df = {}
+
+        # invoke each transform function, passing the relevant dataframe from extracted_data_df as an arg and saving this as a value on a key in transformed_dfs
+        transformed_data_df['sales_order_fact'] = transform_function(extracted_data_df['sales_order'])
+
+        # loop through the dataframes in transformed_dfs, passing the table name, dataframe & target bucket into convert_dataframes_to_parquet to be convert to parquet and save to processed bucket
+        for table_name, dataframe in transformed_data_df.items():
+            convert_dataframe_to_parquet(table_name, dataframe, processed_bucket)
+    except Exception as e:
+        logger.info(e)
