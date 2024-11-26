769ede022d0f7cd800327618faead2e2883588d8
bengriffiths95 <bengriffiths95@gmail.com>
2024-11-26 14:37:30 +0000

    remove util function files from load_lambda/src which will be replaced by new util functions

--- load_lambda/load_lambda.py
+++ load_lambda/load_lambda.py
@@ -3,14 +3,14 @@ import os
 
 if os.environ.get("AWS_EXECUTION_ENV") is not None:
     # For use in lambda function
-    from src.secrets_manager import get_secret
-    from src.db_connection import create_conn
+    # from src.secrets_manager import get_secret
+    # from src.db_connection import create_conn
     from src.load_parquet_data import read_parquet_data_to_dataframe
 
 else:
     # For local use
-    from load_lambda.src.secrets_manager import get_secret
-    from load_lambda.src.db_connection import create_conn
+    # from load_lambda.src.secrets_manager import get_secret
+    # from load_lambda.src.db_connection import create_conn
     from load_lambda.src.load_parquet_data import read_parquet_data_to_dataframe
 
 
@@ -25,10 +25,10 @@ def lambda_handler(event, context):
         logger.info("Passed event: secret=%s, bucket=%s", secret, bucket)
 
         # retrieve db credentials from AWS Secrets Manager 
-        database_credentials_string = get_secret(secret)
+        # database_credentials_string = get_secret(secret)
 
         # create db connection
-        conn = create_conn(database_credentials_string)
+        # conn = create_conn(database_credentials_string)
 
         # retrieve most recent set of parquet files from bucket and return as dataframes
         data = read_parquet_data_to_dataframe(bucket)
