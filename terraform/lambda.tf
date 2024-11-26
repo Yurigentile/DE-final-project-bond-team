16068f97189cba2c076de0b61c6bf01621cc4175
Odashka <darya.babicheva@gmail.com>
2024-11-26 13:39:26 +0000

    change timeout in transform lambda

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -101,7 +101,7 @@ resource "aws_lambda_function" "transform_handler" {
   role          = aws_iam_role.lambda_role.arn
   handler       = "transform_handler.lambda_handler"
   runtime       = var.python_runtime
-  timeout       = 120
+  timeout       = 240
   layers = [
     "arn:aws:lambda:eu-west-2:336392948345:layer:AWSSDKPandas-Python312:14",
     aws_lambda_layer_version.requests_layer_dependencies.arn
